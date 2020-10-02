8c3e795aae268e63bc8761d36c65bf924eeb9bc4
L.G.A.Nethmal <akithanethmal@gmail.com>
2020-10-02 18:12:28 +0530

    cross origin fix

--- src/main/java/AutoWithAll/AutoWithAll/AutoWithAllApplication.java
+++ src/main/java/AutoWithAll/AutoWithAll/AutoWithAllApplication.java
@@ -2,12 +2,28 @@ package AutoWithAll.AutoWithAll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AutoWithAllApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoWithAllApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("OPTIONS","GET","POST","DELETE","PATCH","PUT");
+			}
+		};
 	}
 
 }
--- src/main/java/AutoWithAll/AutoWithAll/security/WebSecurityConfig.java
+++ src/main/java/AutoWithAll/AutoWithAll/security/WebSecurityConfig.java
@@ -18,8 +18,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.validation.constraints.Email;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -55,6 +59,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
@@ -62,15 +79,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .cors()
-                .and()
-                .csrf().disable()
+
+                .csrf().disable().cors().and()
                 .exceptionHandling()
                     .authenticationEntryPoint(unauthorizedHandler)
                 .and()
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .formLogin().loginPage("/login").permitAll().and()
                 .authorizeRequests()
                     .antMatchers("/api/auth/**").permitAll()
                     .antMatchers("/api/test/**").permitAll()
