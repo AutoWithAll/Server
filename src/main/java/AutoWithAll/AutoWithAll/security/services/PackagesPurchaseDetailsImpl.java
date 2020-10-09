86b3f3761c958928a24342d2203036d993da748e
RashiniMaduka <rashinimaduka1029@gmail.com>
2020-10-09 20:51:16 +0530

    'PackagesRepo'

--- src/main/java/AutoWithAll/AutoWithAll/security/services/PackagesPurchaseDetailsImpl.java
+++ src/main/java/AutoWithAll/AutoWithAll/security/services/PackagesPurchaseDetailsImpl.java
@@ -1,5 +1,11 @@
 package AutoWithAll.AutoWithAll.security.services;
+import AutoWithAll.AutoWithAll.models.PackagePurchase;
+import AutoWithAll.AutoWithAll.models.Packages;
+import AutoWithAll.AutoWithAll.repository.PackagesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class PackagesPurchaseDetailsImpl {
+
 }
